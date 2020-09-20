6962c4fd3c7950302b89ef467221b374196a19f9
Abhishek Singh <abhishek3571@gmail.com>
2020-09-20 16:57:00 +0530

    Use @Input and @Output in Angular Fundamentals

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { EventsAppComponent } from './events-app.component';
+import { EventThumbnailComponent } from './events/event-thumbnail.component';
+import { EventsListComponent } from './events/events-list.component';
 
 @NgModule({
-  declarations: [
-    EventsAppComponent,
-  ],
   imports: [
     BrowserModule,
   ],
+  declarations: [
+    EventsAppComponent,
+    EventsListComponent,
+    EventThumbnailComponent
+  ],
+
   // tslint:disable-next-line: object-literal-sort-keys
   bootstrap: [EventsAppComponent],
 })
