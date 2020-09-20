67f7aee0844e1a2fd0d3614a10ea73f4f9cc729a
Abhishek Singh <abhishek3571@gmail.com>
2020-09-20 08:37:28 +0530

    Completed Getting Started with angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import { EventsAppComponent } from './events-app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    EventsAppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  // tslint:disable-next-line: object-literal-sort-keys
+  bootstrap: [EventsAppComponent],
 })
 export class AppModule { }
